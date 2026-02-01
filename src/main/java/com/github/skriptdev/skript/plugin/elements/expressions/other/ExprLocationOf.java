83b1506868f17f34b1a0c4de115d6901f8e21339
ShaneBeee <shanebolenback@me.com>
2026-02-01 09:15:04 -0800

    ExprLocationOf - revert back to property expression - Fixed an issue in the parser so this will now work

--- src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/other/ExprLocationOf.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/other/ExprLocationOf.java
@@ -5,21 +5,16 @@ import com.hypixel.hytale.math.vector.Location;
 import com.hypixel.hytale.server.core.entity.Entity;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.world.World;
-import io.github.syst3ms.skriptparser.lang.Expression;
-import io.github.syst3ms.skriptparser.lang.TriggerContext;
-import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import io.github.syst3ms.skriptparser.lang.properties.PropertyExpression;
 import io.github.syst3ms.skriptparser.registration.SkriptRegistration;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ExprLocationOf implements Expression<Location> {
+public class ExprLocationOf extends PropertyExpression<Object, Location> {
 
     public static void register(SkriptRegistration registration) {
-        registration.newExpression(ExprLocationOf.class, Location.class, false,
-                "location[s] of %blocks/entities%",
-                "%entities/blocks%'[s] location[s]")
+        registration.newPropertyExpression(ExprLocationOf.class, Location.class,
+                "location", "blocks/entities")
             .name("Location of Block/Entity")
             .description("Returns the location of a block or entity.")
             .examples("set {_loc} to location of context-player")
@@ -27,40 +22,18 @@ public class ExprLocationOf implements Expression<Location> {
             .register();
     }
 
-    private Expression<?> objects;
-
-    @Override
-    public boolean init(Expression<?>[] expressions, int matchedPattern, @NotNull ParseContext parseContext) {
-        this.objects = expressions[0];
-        return true;
-    }
-
     @SuppressWarnings("removal")
     @Override
-    public Location[] getValues(@NotNull TriggerContext ctx) {
-        List<Location> locations = new ArrayList<>();
-        for (Object o : this.objects.getArray(ctx)) {
-            if (o instanceof Entity entity) {
-                World world = entity.getWorld();
-                assert world != null;
-                TransformComponent transform = entity.getTransformComponent();
-                locations.add(new Location(world.getName(), transform.getPosition()));
-            } else if (o instanceof Block block) {
-                locations.add(block.getLocation());
-            }
+    public @Nullable Location getProperty(@NotNull Object owner) {
+        if (owner instanceof Entity entity) {
+            World world = entity.getWorld();
+            assert world != null;
+            TransformComponent transform = entity.getTransformComponent();
+            return new Location(world.getName(), transform.getPosition());
+        } else if (owner instanceof Block block) {
+            return block.getLocation();
         }
-        return locations.toArray(new Location[0]);
-    }
-
-    @Override
-    public boolean isSingle() {
-        return this.objects.isSingle();
-    }
-
-    @Override
-    public String toString(@NotNull TriggerContext ctx, boolean debug) {
-        String plural = this.objects.isSingle() ? "" : "s";
-        return "location" + plural + " of " + this.objects.toString(ctx, debug);
+        return null;
     }
 
 }
